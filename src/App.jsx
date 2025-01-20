f591ab8c14a703e4288598538ba829e80d5f5846
offor <offorchukwuebuka3@gmail.com>
2025-01-20 20:23:19 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
 		<>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
-				<Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
-				<Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
+				<Route path='/login' element={user ? <LoginPage /> : <Navigate to={"/"} />} />
+				<Route path='/signup' element={user ? <SignUpPage /> : <Navigate to={"/"} />} />
 				<Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
 				<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
 				<Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
