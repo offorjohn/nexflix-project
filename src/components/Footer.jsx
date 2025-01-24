7e45dfd3d5df6fb0d3ec2e386df5741a3178131b
offor <offorchukwuebuka3@gmail.com>
2025-01-24 23:40:18 +0100

    added clear logo and made footer more bigger

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 const Footer = () => {
 	return (
-		<footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t-4 border-gray-800"  style={{ borderTop: "20px solid #1f2937" }}>
+		<footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t-4 border-gray-800" style={{ borderTop: "20px solid #1f2937" }}>
 			<div className="flex flex-col items-center justify-center gap-4 md:h-24">
-				<p className="text-center text-sm leading-loose font-bold text-white">
-					All rights reserved Mazaia | 2025
+				<p
+					className="text-center text-sm leading-loose font-bold text-white uppercase"
+					style={{ fontWeight: 900 }}
+				>
+					ALL RIGHTS RESERVED MAZAIA | 2025
 				</p>
+
 			</div>
 		</footer>
 	);
